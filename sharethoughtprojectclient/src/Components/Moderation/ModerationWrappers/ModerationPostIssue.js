aae1f28da43f6b42d5e978bb4281858e15328389
Szymon Szalata <SZYMON.SZALATA@pb.com>
2022-03-26 21:30:34 +0100

    Configured moderation issue for post

---
+++ sharethoughtprojectclient/src/Components/Moderation/ModerationWrappers/ModerationPostIssue.js
@@ -0,0 +1,64 @@
+import { Button, Card, Col, Row, Modal, ButtonGroup } from "react-bootstrap";
+import AuthorInfoAndCreateDate from "../../Home/PostsContainers/SinglePostComponent/AuthorInfoAndCreationDate";
+import '../Moderation.css';
+import React, { useEffect, useState } from "react";
+
+const ModerationPostIssue = (props) => {
+    const [show, setShow] = useState(false);
+    const [rerender, reRender] = useState(0);
+    const rerenderStuff = () => reRender(rerender + 1);
+    const handleClose = () => {
+        setShow(false);
+        reRender(rerender + 1);
+    }
+    const handleShow = () => {
+        setShow(true); 
+        reRender(rerender + 1);
+    }
+    return (
+        <div className="new-post">
+            <Row>
+                <Col md={4}>
+                    <a className="hyperlink-card" href="/post/1">
+                        <Card.Img variant="top" src={props.postPic} />
+                    </a>
+                </Col>
+                <Col>
+                    <Row>
+                        <Card.Title className="custom-card-title">{props.title}</Card.Title>
+                        <AuthorInfoAndCreateDate showInfo={props.showInfo} authorName={props.authorName} authorPic={props.authorPic}></AuthorInfoAndCreateDate>
+                        <Card.Text>{props.desc}</Card.Text>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <div>Reported: 23.02.2021</div>
+                            <div>Reported for: {props.reason}</div>
+                            <div>Status: {rerender}</div>
+                        </Col>
+                        <Col>
+                            <Button variant="warning" className="resolve-button" onClick={handleShow}>Resolve</Button>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                    <ButtonGroup>
+                        <Button variant="outline-success" onClick={handleClose}>
+                            Mark as safe
+                        </Button>
+                        <Button variant="outline-danger" onClick={handleClose} style={{marginLeft: '0px', borderRadius: '0'}}>
+                            Remove
+                        </Button>
+                    </ButtonGroup>
+                </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default ModerationPostIssue;
--- sharethoughtprojectclient/src/Components/Moderation/ModerationWrappers/ModerationPostWrapper.js
+++ sharethoughtprojectclient/src/Components/Moderation/ModerationWrappers/ModerationPostWrapper.js
@@ -1,6 +1,7 @@
 import { Component } from "react";
 import { Button, Container } from "react-bootstrap";
 import HorizontalPostList from "../../Home/PostsContainers/HorizontalPostList";
+import ModerationPostIssue from "./ModerationPostIssue";
 
 class ModerationPostWrapper extends Component {
     constructor(props) {
@@ -32,17 +33,20 @@ class ModerationPostWrapper extends Component {
         if (this.state.resolved) {
             return (
                 <div>
-                    <HorizontalPostList postAmount={5}>
-
-                    </HorizontalPostList>
+                    <ModerationPostIssue
+                        showInfo={true}
+                        authorPic={"https://prsmeble.pl/wp-content/uploads/2013/10/300x201xperson.jpg.pagespeed.ic.jkTJYjUU9H.webp"}
+                        postPic={"https://i0.wp.com/konsolowe.info/wp-content/uploads/2021/08/lir-usp-in-engine-screen-twotown-royam-no-logo.jpg.adapt_.crop16x9.1455w-1.jpg?fit=1455%2C818&ssl=1"}
+                        authorName={"Dżeremaja Kutahaja"}
+                        reason={"Dumb name lol"}
+                        desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean vestibulum libero vitae venenatis scelerisque. Proin mattis in leo eu tempus. Morbi nec pulvinar eros, at vulputate quam. Fusce a vestibulum ex, id aliquam leo."
+                    ></ModerationPostIssue>
                 </div>);
         }
         else {
             return (
                 <div>
-                    <HorizontalPostList postAmount={3}>
-
-                    </HorizontalPostList>
+                    <ModerationPostIssue></ModerationPostIssue>
                 </div>);
         }
     }
