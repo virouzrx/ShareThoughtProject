548e5f4d6f921aaf4ca97f6bd99c4c7ff7bb5956
Szymon Szalata <SZYMON.SZALATA@pb.com>
2022-03-30 22:40:46 +0200

    Configured register with redirect.

--- sharethoughtprojectclient/src/Components/AuthComponents/AuthSubComponents/Register.js
+++ sharethoughtprojectclient/src/Components/AuthComponents/AuthSubComponents/Register.js
@@ -1,30 +1,72 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
 import '../Auth.css';
+import axios from 'axios';
+import { Navigate } from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
+import { createBrowserHistory } from "history";
+
+function SendRegisterRequest(email, username, password) {
+    const history = createBrowserHistory({forceRefresh:true});
+    var body = {
+        email: email,
+        username: username,
+        password: password
+    };
+
+    axios.post('https://localhost:5001/api/v1/identity/register', body)
+        .then(function (response) {
+            history.push('/posts');
+            document.location.reload()
+        })
+        .catch(function (error) {
+            document.getElementById("errorMessage").innerHTML = error.response.data.errors[0];
+        });
+}
 
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            value: '',
-            password: ''
+            email: '',
+            username: '',
+            password: '',
+            cpassword: '',
         };
 
         this.handleEmailChange = this.handleEmailChange.bind(this);
         this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleUsernameChange = this.handleUsernameChange.bind(this);
+        this.handleCpasswordChange = this.handleCpasswordChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleEmailChange = (event) => {
-        this.setState({ value: event.target.value });
+        this.setState({ email: event.target.value });
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({ username: event.target.value });
     }
 
     handlePasswordChange = (event) => {
         this.setState({ password: event.target.value });
     }
 
+    handleCpasswordChange = (event) => {
+        this.setState({ cpassword: event.target.value });
+    }
+
     handleSubmit = (event) => {
-        alert('Login: ' + this.state.value + '\r\nPassword: ' + this.state.password);
+        event.preventDefault();
+        document.getElementById("errorMessage").innerHTML = "";
+        if (this.state.password != this.state.cpassword) {
+            document.getElementById("errorMessage").innerHTML = "Passwords don't match!";
+        }
+        else {
+            SendRegisterRequest(this.state.email, this.state.username, this.state.password);
+        }
+
     }
 
     render() {
@@ -35,7 +77,7 @@ class Register extends React.Component {
                         <Form.Label>Email</Form.Label>
                         <Form.Control
                             type="email"
-                            value={this.state.value}
+                            value={this.state.email}
                             onChange={this.handleEmailChange}
                         />
                     </Form.Group>
@@ -43,27 +85,28 @@ class Register extends React.Component {
                         <Form.Label>Username</Form.Label>
                         <Form.Control
                             type="text"
-                            value={this.state.value}
-                            onChange={this.handleEmailChange}
+                            value={this.state.username}
+                            onChange={this.handleUsernameChange}
                         />
                     </Form.Group>
                     <Form.Group size="lg" controlId="email">
                         <Form.Label>Password</Form.Label>
-                        <Form.Control
-                            type="email"
-                            value={this.state.value}
-                            onChange={this.handleEmailChange}
-                        />
-                    </Form.Group>
-                    <Form.Group size="lg" controlId="password">
-                        <Form.Label>Confirm password</Form.Label>
                         <Form.Control
                             type="password"
                             value={this.state.password}
                             onChange={this.handlePasswordChange}
                         />
                     </Form.Group>
-                    <Button block size="lg" type="submit" >
+                    <Form.Group size="lg" controlId="password">
+                        <Form.Label>Confirm password</Form.Label>
+                        <Form.Control
+                            type="password"
+                            value={this.state.cpassword}
+                            onChange={this.handleCpasswordChange}
+                        />
+                    </Form.Group>
+                    <p id="errorMessage"></p>
+                    <Button size="lg" type="submit" >
                         Login
                     </Button>
                 </Form>
