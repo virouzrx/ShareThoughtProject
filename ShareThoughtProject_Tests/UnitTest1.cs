2a44d73876030ad9e53bf83534fc887b2b5a4674
Szymon Szałata <50450702+virouzrx@users.noreply.github.com>
2022-07-03 17:45:05 +0200

    Added test project

---
+++ ShareThoughtProject_Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace ShareThoughtProject_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
